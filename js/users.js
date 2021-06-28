463dbde1333e9391d6e8953fc3a26139185ce1ca
Eric Harrison <github@hrrsn.co>
2021-06-28 09:30:31 -0400

    first commit

---
+++ js/users.js
@@ -0,0 +1,5 @@
+
+// On page load, pull the users endpoint from https://jsonplaceholder.typicode.com/users using XHR
+// Generate the table of users for #usersTable (include a button/link in the fourth column to let you view an individual record)
+// If a button/link in the table is clicked, populate #userView with the full details of the user.
+// (bonus points if you make it a nice form as if the user was editable)
