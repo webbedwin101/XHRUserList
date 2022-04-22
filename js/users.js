1a105688bf72c9ae6a9eca325f65fb4f6c36ee12
webbe32 <webb.edwin@gmail.com>
2022-04-22 09:48:08 -0400

    hw6

--- js/users.js
+++ js/users.js
@@ -3,3 +3,26 @@
 // Generate the table of users for #usersTable (include a button/link in the fourth column to let you view an individual record)
 // If a button/link in the table is clicked, populate #userView with the full details of the user.
 // (bonus points if you make it a nice form as if the user was editable)
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(json => console.log(json)) 
+
+// function reqListener () {
+//     console.log(this.responseText);
+// }
+
+// var oReq = new XMLHttpRequest();
+// oReq.addEventListener("load", reqListener);
+// oReq.open("GET", "https://jsonplaceholder.typicode.com/users");
+// oReq.send();
+
+function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        document.getElementById("userList").innerHTML =
+        this.responseText;
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhttp.send();
+}
